e0b9d536349c39e29fc98bfdcb312dbf89c8296a
rini <rinimariam94@gmail.com>
2022-05-31 18:41:02 +0530

    Add methods

---
+++ src/main/java/org/divisions/DivisionTransformer.java
@@ -0,0 +1,14 @@
+package org.divisions;
+
+import java.util.List;
+
+import org.mapstruct.Mapper;
+import org.util.QuarkusMappingConfig;
+
+@Mapper(config = QuarkusMappingConfig.class)
+
+public interface DivisionTransformer {
+   Division toResource(DivisionEntity divisionEntity);
+    List<Division> map(List<DivisionEntity> divisionEntityList);
+    DivisionEntity toEntity(Division division);
+}
